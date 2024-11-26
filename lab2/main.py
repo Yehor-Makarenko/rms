2ea2108c8cba5a42e3f87c5e837e6966eb0ea2be
Yehor Makarenko <mak.yehor.knu@gmail.com>
2024-11-26 10:32:56 +0200

    Fix

--- lab2/main.py
+++ lab2/main.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 import random
 
 def data_to_midi_notes(data):
-    min_note = 60  # Мінімальна нота (Середнє "До")
-    max_note = 72  # Максимальна нота
+    min_note = 60 
+    max_note = 72
     min_data, max_data = min(data), max(data)
 
-    # Нормалізація даних до діапазону нот
     notes = [
         int(min_note + (value - min_data) * (max_note - min_note) / (max_data - min_data))
         for value in data
@@ -20,15 +19,11 @@ notes = data_to_midi_notes(data)
 midi = MidiFile()
 track = MidiTrack()
 midi.tracks.append(track)
-
-# Додавання нот до MIDI-файлу
 for note in notes:
-    track.append(Message('note_on', note=note, velocity=64, time=200))  # Нота вмикається
-    track.append(Message('note_off', note=note, velocity=64, time=200))  # Нота вимикається
-
-# Збереження файлу
+    track.append(Message('note_on', note=note, velocity=64, time=200))
+    track.append(Message('note_off', note=note, velocity=64, time=200)) 
+    
 midi.save('output.mid')
-print("MIDI-файл збережено як 'output.mid'")
 
 plt.plot(data, label="Дані")
 plt.scatter(range(len(notes)), notes, color='red', label="MIDI-ноти")
