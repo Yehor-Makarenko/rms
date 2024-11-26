39f9f6e804554c0dc286334d5be3d2d24be7ef6e
Yehor Makarenko <mak.yehor.knu@gmail.com>
2024-11-26 11:23:20 +0200

    Fix midi

--- lab2/main.py
+++ lab2/main.py
@@ -8,27 +8,25 @@ def data_to_midi_notes(data):
     min_data, max_data = min(data), max(data)
 
     notes = [
-        int(min_note + (value - min_data) * (max_note - min_note) / (max_data - min_data))
-        for value in data
+        (int(min_note + (value - min_data) * (max_note - min_note) / (max_data - min_data)), int(dur * 1000))
+        for (value, dur) in data
     ]
     return notes
 
-data = [random.randint(0, 100) for _ in range(10)]
+data = [(60, 0.5), (62, 0.5), (64, 0.5), (65, 0.5), (67, 0.5), 
+(69, 0.5), (67, 0.5), (65, 0.5), (64, 0.5), (62, 0.5), 
+(60, 0.7), (62, 0.5), (64, 0.5), (67, 0.5), 
+(69, 0.5), (71, 0.5), (69, 0.5), (67, 0.5), (65, 0.5), 
+(64, 0.7), (65, 0.5), 
+(64, 0.5), (62, 0.5), (60, 0.5), (62, 0.5), (64, 0.5), 
+(65, 0.5), (67, 0.7), (65, 0.5), (64, 0.5), (62, 0.5), (60, 1)]
 notes = data_to_midi_notes(data)
 
 midi = MidiFile()
 track = MidiTrack()
 midi.tracks.append(track)
-for note in notes:
-    track.append(Message('note_on', note=note, velocity=64, time=200))
-    track.append(Message('note_off', note=note, velocity=64, time=200)) 
+for note, dur in notes:
+    track.append(Message('note_on', note=note, velocity=64, time=20))
+    track.append(Message('note_off', note=note, velocity=64, time=dur))     
     
 midi.save('output.mid')
-
-plt.plot(data, label="Дані")
-plt.scatter(range(len(notes)), notes, color='red', label="MIDI-ноти")
-plt.legend()
-plt.title("Соніфікація даних")
-plt.xlabel("Індекс")
-plt.ylabel("Значення/Ноти")
-plt.show()
